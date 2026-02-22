095f8db49bd0ba3e81186e8ce3a5d5127c34afdb
Meghana <meghanab0047@gmail.com>
2026-02-22 13:20:15 +0530

    Change uvicorn run parameters for app execution

--- backend/main.py
+++ backend/main.py
@@ -169,4 +169,4 @@ async def root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="8000", reload = True)
+    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
